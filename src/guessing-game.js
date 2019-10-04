3030f742394b84a46427d33d52bfb83382f01780
truhin-andrei <truhin-andrei@bk.ru>
2019-10-04 19:33:00 +0700

    feat: add solution for task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.round((this.minValue+this.maxValue)/2);
     }
 
     lower() {
-
+        this.maxValue = this.guess();
     }
 
     greater() {
-
+        this.minValue = this.guess();
     }
 }
 
